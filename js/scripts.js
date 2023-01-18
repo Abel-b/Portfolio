5ee3d1dfa0baf07e69fd8863cf824eb65d387981
Abel Berhane <abelberhane011@gmail.com>
2023-01-18 12:11:23 +0100

    updates the projects section

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,7 @@
 
         // Add icons to each block
         $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-university"></i></div>');
         });
 
         // Add dates to the timeline if exists
